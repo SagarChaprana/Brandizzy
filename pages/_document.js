9fb16bc7709cec40876a8e19909e468fbdda9f1b
Pavan <pavansargar36@gmail.com>
2023-02-04 23:30:43 +0530

    configured/netlify

--- pages/_document.js
+++ pages/_document.js
@@ -5,15 +5,38 @@ export default function Document() {
     <Html>
       <Head>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin="true"
+        />
         <link
           href="https://fonts.googleapis.com/css2?family=Karla:wght@300;400;500;600;700&family=Lato:ital,wght@0,300;0,400;0,900;1,400;1,700&display=swap"
           rel="stylesheet"
+        />
+        <script
+          async
+          src="https://identity.netlify.com/v1/netlify-identity-widget.js"
         />
       </Head>
       <body>
         <Main />
         <NextScript />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              if (window.netlifyIdentity) {
+                window.netlifyIdentity.on("init", user => {
+                  if (!user) {
+                    window.netlifyIdentity.on("login", () => {
+                      document.location.href = "/admin/";
+                    });
+                  }
+                });
+              }
+          `,
+          }}
+        />
       </body>
     </Html>
   );
--- pages/index.jsx
+++ pages/index.jsx
@@ -1,15 +1,57 @@
-import Head from 'next/head'
-import Image from 'next/image'
-import styled from 'styled-components'
+import Head from "next/head";
+import fs from "fs";
+import matter from "gray-matter";
+import Link from "next/link";
+import Image from "next/image";
+import styled from "styled-components";
 
-export default function Home() {
+export default function Home({ products }) {
   return (
     <Container>
-      <h1>hello there mate!</h1>
+      <Head>
+        <title>Demo Blog</title>
+      </Head>
+      <h1>Welcome to my blog</h1>
+      <p>This is a subtitle idk what to type here</p>
+      <ul>
+        {products.map((product) => (
+          <li key={product.slug}>
+            <Link href={`/product/${product.slug}`}>
+              {product.date}:{product.title}
+            </Link>
+          </li>
+        ))}
+      </ul>
     </Container>
-  )
+  );
 }
 
-
 const Container = styled.div`
-`
+  height: 100vh;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+`;
+
+export async function getStaticProps() {
+  // List of files in blgos folder
+  const filesInBlogs = fs.readdirSync("./content/products");
+
+  // Get the front matter and slug (the filename without .md) of all files
+  const products = filesInBlogs.map((filename) => {
+    const file = fs.readFileSync(`./content/products/${filename}`, "utf8");
+    const matterData = matter(file);
+
+    return {
+      ...matterData.data, // matterData.data contains front matter
+      slug: filename.slice(0, filename.indexOf(".")),
+    };
+  });
+
+  return {
+    props: {
+      products,
+    },
+  };
+}
